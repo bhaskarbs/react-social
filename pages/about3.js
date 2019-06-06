be5e3c347741fa24c73d131f8306b507b656bfe0
Bhaskar <bhaskar.somashekar@anz.com>
2019-06-06 12:32:31 +0530

    initial commit

---
+++ pages/about3.js
@@ -0,0 +1,7 @@
+import Layout from '../components/layout3.js';
+
+const aboutPageContent = <p>This is the about page</p>;
+
+export default function About() {
+    return <Layout content={aboutPageContent} />;
+}
---
+++ pages/index3.js
@@ -0,0 +1,7 @@
+import Layout from '../components/layout3.js';
+
+const indexPageContent = <p>Hello Next.js</p>;
+
+export default function Index() {
+    return <Layout content={indexPageContent} />;
+}
